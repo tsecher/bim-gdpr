f15bc612749156513014e4d87454623550a08ff5
tsecher <me@thomas-secher.fr>
2020-04-03 13:49:07 +0200

    Add default placeholder initialisation call

--- src/core/services/ServicePlaceholder.js
+++ src/core/services/ServicePlaceholder.js
@@ -1,6 +1,7 @@
 import { PREFIX } from '../tools/Tools'
 import { Core } from '../Core'
 import { ServiceEvents } from './ServiceEvents'
+import { Service } from 'easy-gdpr/src/core/services/Service';
 
 export class ServicePlaceholder{
 
@@ -49,30 +50,64 @@ export class ServicePlaceholder{
         // Show enabled placeholder
         const selectorOn = `${PREFIX}visible-if-enabled`
         document.querySelectorAll(`[${selectorOn}]`)
-            .forEach( element => this.initDOMItem(element, selectorOn, this.showElement, this.hideElement) )
+            .forEach( element => {
+                const service = this.getServiceFromElement(element, selectorOn)
+                this.initDOMItem(element, service, this.showElement, this.hideElement)
+             } )
 
         // Hide enabled placeholder
         const selectorOff = `${PREFIX}hidden-if-enabled`
         document.querySelectorAll(`[${selectorOff}]`)
-            .forEach( element => this.initDOMItem(element, selectorOff, this.hideElement, this.showElement) )
+            .forEach( element => {
+                const service = this.getServiceFromElement(element, selectorOff)
+                this.initDefaultPlacehoder(element, service)
+                this.initDOMItem(element, service, this.hideElement, this.showElement)
+             })
     }
 
     /**
      * Init a dom element according to service.
      *
      * @param {DOMElement} element 
-     * @param {string} selector 
+     * @param {Service} selector 
      * @param {function} enabledCallback 
      * @param {function} disabledCallback 
      */
-    initDOMItem( element, selector, enabledCallback, disabledCallback ){
-        const service = Core.getServiceById(element.getAttribute(selector))
+    initDOMItem( element, service, enabledCallback, disabledCallback ){
         if( service && service.isEnabled() ){
             enabledCallback(element)
         }
         else{
             disabledCallback(element)
         }
+    }
+
+    /**
+     * Return the service related to the element
+     * 
+     * @param {Element} element 
+     * @param {string} selector 
+     */
+    getServiceFromElement(element, selector){
+        return Core.getServiceById(element.getAttribute(selector))
+    }
+
+    /**
+     * Add a default placeholder if needed
+     *
+     * @param {Element} element 
+     * @param {*} service 
+     */
+    initDefaultPlacehoder(element, service){
+        if(element.innerHTML.trim().length == 0){
+            try {                
+                const content = Core.getTemplate().initDefaultPlaceholder(element, service)
+                if( content ){
+                    element.innerHTML = content
+                }
+            } catch (error) {   
+            }
+        }        
     }
 
     /**
