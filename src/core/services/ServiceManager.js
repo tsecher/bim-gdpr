5a028ebb3cbfea173e5d85cd2fc48ccf806abed6
tsecher <me@thomas-secher.fr>
2020-04-10 13:07:03 +0200

    [core/services] Fix multiple enable triggering

--- src/core/services/ServiceManager.js
+++ src/core/services/ServiceManager.js
@@ -1,9 +1,9 @@
-import { Service } from "./Service"
-import { ServiceEvents } from "./ServiceEvents"
-import { ServicePlaceholder } from "./ServicePlaceholder"
-import { sortByWeight, checkInterface, PREFIX } from "../tools/Tools"
-import { Core } from "../Core"
-import { ServiceStatusManager, ServiceStatus, Cookies} from "./ServiceStatusManager"
+import {Service} from "./Service"
+import {ServiceEvents} from "./ServiceEvents"
+import {ServicePlaceholder} from "./ServicePlaceholder"
+import {sortByWeight, checkInterface, PREFIX} from "../tools/Tools"
+import {Core} from "../Core"
+import {ServiceStatusManager, ServiceStatus, Cookies} from "./ServiceStatusManager"
 
 /**
  * The service interface.
@@ -12,15 +12,16 @@ const ServiceInterface = {
     id: 'id',
     name: 'Name',
     description: 'description',
-    start: function(){},
+    start: function () {
+    },
 }
 
 /**
  * The service manager.
  */
-class ServiceManagerClass{
+class ServiceManagerClass {
 
-    constructor(){
+    constructor() {
         this.services = []
         this.servicePlaceholder = new ServicePlaceholder()
     }
@@ -28,15 +29,15 @@ class ServiceManagerClass{
     /**
      * Return the list of services.
      */
-    getServicesList(){
+    getServicesList() {
         return this.services
     }
 
     /**
      * Return the list of enabled services.
      */
-    getEnabledServicesList(){
-        return this.services.filter( service => {
+    getEnabledServicesList() {
+        return this.services.filter(service => {
             return service.isEnabled()
         })
     }
@@ -44,8 +45,8 @@ class ServiceManagerClass{
     /**
      * Return the list of disabled services
      */
-    getDisabledServicesList(){
-        return this.services.filter( service => {
+    getDisabledServicesList() {
+        return this.services.filter(service => {
             return service.isDisabled()
         })
     }
@@ -53,8 +54,8 @@ class ServiceManagerClass{
     /**
      * Return the list of pending services
      */
-    getPendingServices(){
-        return this.services.filter( service => {
+    getPendingServices() {
+        return this.services.filter(service => {
             return service.isPending()
         })
     }
@@ -62,8 +63,8 @@ class ServiceManagerClass{
     /**
      * Return the list of mandatory services.
      */
-    getMandatoryServicesList(){
-        return this.services.filter( service => {
+    getMandatoryServicesList() {
+        return this.services.filter(service => {
             return service.isMandatory()
         })
     }
@@ -71,45 +72,44 @@ class ServiceManagerClass{
     /**
      * Init the default behaviors
      */
-    init(){
+    init() {
         this.servicePlaceholder.init();
-        Core.on( ServiceEvents.serviceListHasChanged ).subscribe( (data) => this.sortServices() )
+        Core.on(ServiceEvents.serviceListHasChanged).subscribe((data) => this.sortServices())
     }
-    
+
     /**
      * Add a service
-     * 
-     * @param {object} serviceData 
+     *
+     * @param {object} serviceData
      */
-    createService(serviceData){
+    createService(serviceData) {
         let service = serviceData
 
         // Check if service is a class.
-        if( !(this.isService(serviceData))){
+        if (!(this.isService(serviceData))) {
             service = this.overrideService(serviceData)
-            if( !service ){
+            if (!service) {
                 return null
             }
         }
 
-        if( !this.getServiceById(service.id) ){
+        if (!this.getServiceById(service.id)) {
             // Add weight to service
-            service.weight = typeof(serviceData.weight) !== 'undefined'  ? service.weight : this.services.length
+            service.weight = typeof (serviceData.weight) !== 'undefined' ? service.weight : this.services.length
             service.init()
             this.services.push(service)
 
             // Init enabled services.
-            if( service.isEnabled() ){
+            if (service.isEnabled()) {
                 this.startService(service)
             }
 
             // Dispatch event.
-            Core.trigger( ServiceEvents.serviceListHasChanged, {
-                    services: this.services
-                })
+            Core.trigger(ServiceEvents.serviceListHasChanged, {
+                services: this.services
+            })
             return service
-        }
-        else {
+        } else {
             throw `Service ${id} already exists`
         }
         return null
@@ -117,29 +117,29 @@ class ServiceManagerClass{
 
     /**
      * Return a service from obj that match Service Interface
-     * @param {*} serviceData 
+     * @param {*} serviceData
      */
-    overrideService(serviceData){
+    overrideService(serviceData) {
         let service = null
-        if( checkInterface( ServiceInterface, serviceData, Core.logsAreEnabled()) ){
+        if (checkInterface(ServiceInterface, serviceData, Core.logsAreEnabled())) {
             service = new Service()
             // Pseudo extension from object.
-            for( let i in serviceData){
+            for (let i in serviceData) {
                 service[i] = serviceData[i]
             }
 
             // Proto
-            if(  Object.getOwnPropertyNames(Object.getPrototypeOf(serviceData)).indexOf('start') > -1 ){
+            if (Object.getOwnPropertyNames(Object.getPrototypeOf(serviceData)).indexOf('start') > -1) {
                 Object.getOwnPropertyNames(Object.getPrototypeOf(serviceData)).map(
                     propertyName => {
                         service[propertyName] = serviceData[propertyName]
                     }
                 )
 
-                if( 'function' === typeof(serviceData.getDefaultTranslations) ){
-                    service.setTranslations( serviceData.getDefaultTranslations() )
+                if ('function' === typeof (serviceData.getDefaultTranslations)) {
+                    service.setTranslations(serviceData.getDefaultTranslations())
                 }
-            }            
+            }
         }
         return service
     }
@@ -147,11 +147,11 @@ class ServiceManagerClass{
     /**
      * Return a service by id.
      *
-     * @param {string} id 
-     * 
+     * @param {string} id
+     *
      * @returns {Service}
      */
-    getServiceById(id){
+    getServiceById(id) {
         return this.services.filter(service => {
             return service.id === id
         })[0]
@@ -160,43 +160,51 @@ class ServiceManagerClass{
     /**
      * Sort services
      */
-    sortServices(){
-        if( this.services.length > 1 ){
-            this.services = this.services.sort((a,b) => sortByWeight(a,b))            
+    sortServices() {
+        if (this.services.length > 1) {
+            this.services = this.services.sort((a, b) => sortByWeight(a, b))
         }
         return this
     }
 
     /**
      * Return true if data is a service.
-     * @param {*} serviceData 
+     * @param {*} serviceData
      */
-    isService(serviceData){
+    isService(serviceData) {
         return serviceData instanceof Service
     }
 
     /**
      * Enable all services.
      */
-    enableAll(){
+    enableAll() {
         this.enableService(this.services)
     }
 
     /**
      * Enable the service
-     * 
-     * @param {Service} service 
+     *
+     * @param {Service} service
      */
-    enableService(service, dispacth=true){
-        if(Array.isArray(service)){
-            service.map( s => { this.enableService(s, false)})
-            Core.trigger(ServiceEvents.serviceStatusHasChanged, {services: service})
-        }
-        else if( !service.isEnabled() ){
+    enableService(service, dispacth = true) {
+        if (Array.isArray(service)) {
+            // Treat only enabled services.
+            const servicesToEnable = service.filter(s => {
+                return !s.isEnabled()
+            })
+
+            if (servicesToEnable.length) {
+                servicesToEnable.map(s => {
+                    this.enableService(s, false)
+                })
+                Core.trigger(ServiceEvents.serviceStatusHasChanged, {services: servicesToEnable})
+            }
+        } else if (service && !service.isEnabled()) {
             ServiceStatusManager.setServiceStatus(service, ServiceStatus.enabled)
             service.onEnable()
             this.startService(service)
-            if( dispacth ){
+            if (dispacth) {
                 Core.trigger(ServiceEvents.serviceStatusHasChanged, {services: [service]})
             }
         }
@@ -205,16 +213,15 @@ class ServiceManagerClass{
     /**
      * Start the service.
      *
-     * @param {Service} service 
+     * @param {Service} service
      */
-    startService(service){
+    startService(service) {
         service.getRelatedScripts().map(script => {
-            if( typeof(script) === 'string'){
+            if (typeof (script) === 'string') {
                 Core.addScript(script)
-            }
-            else{
+            } else {
                 try {
-                    if( script.path ){
+                    if (script.path) {
                         Core.addScript(script.path, script.callback)
                     }
                 } catch (error) {
@@ -230,102 +237,108 @@ class ServiceManagerClass{
     /**
      * Disable all
      */
-    disableAll(){
+    disableAll() {
         this.disableService(this.services)
     }
 
     /**
      * Disable the service
-     * 
-     * @param {Service} service 
+     *
+     * @param {Service} service
      */
-    disableService(service, dispatch=true){
-        if( Array.isArray(service)){
-            service.map(s=>{this.disableService(s, false)})
-            Core.trigger(ServiceEvents.serviceStatusHasChanged, {services: service})
-        }
-        else{
+    disableService(service, dispatch = true) {
+        if (Array.isArray(service)) {
+            // Treat only enabled services.
+            const servicesToDisable = service.filter(s => {
+                return s.isEnabled()
+            })
+
+            if (servicesToDisable.length > 0) {
+                servicesToDisable.forEach(s => {
+                    this.disableService(s, false)
+                })
+                Core.trigger(ServiceEvents.serviceStatusHasChanged, {services: servicesToDisable})
+            }
+        } else if (service) {
             const serviceIsEnabled = service.isEnabled()
-            if( ( serviceIsEnabled || service.isPending()) && !service.isMandatory()){
-                if(service.isEnabled()){
+            if ((serviceIsEnabled || service.isPending()) && !service.isMandatory()) {
+                if (service.isEnabled()) {
                     this.stopService(service)
                     service.onDisable()
                 }
                 ServiceStatusManager.setServiceStatus(service, ServiceStatus.disabled)
-                if( dispatch ){
+                if (dispatch) {
                     Core.trigger(ServiceEvents.serviceStatusHasChanged, {services: [service]})
                 }
             }
-        } 
+        }
     }
 
     /**
      * Start the service.
      *
-     * @param {Service} service 
+     * @param {Service} service
      */
-    stopService(service){
+    stopService(service) {
         service.stop()
-        
-         // Delete cookies.
-         this.deleteCookies(service)
 
-         // Delete local storage
-         this.deleteLocalStorage(service)
-         
+        // Delete cookies.
+        this.deleteCookies(service)
+
+        // Delete local storage
+        this.deleteLocalStorage(service)
+
         // Delete scripts.
-        service.getRelatedScripts().map(script => { 
-            if( typeof(script) === 'string'){
+        service.getRelatedScripts().map(script => {
+            if (typeof (script) === 'string') {
                 Core.removeScript(script)
-            }
-            else if( script.path ){
+            } else if (script.path) {
                 Core.removeScript(script.path)
             }
         })
-        
+
         Core.trigger(ServiceEvents.serviceStop, {service: service})
     }
-
 
 
     /**
      * Delete cookies linked to the service.
      *
-     * @param {Service} service 
+     * @param {Service} service
      */
-    deleteCookies(service){
+    deleteCookies(service) {
         try {
             const listOfCookies = Object.keys(Cookies.get())
             const cookiesPatterns = service.getCookiePatterns()
-            if( listOfCookies.length && cookiesPatterns.length ){
+            if (listOfCookies.length && cookiesPatterns.length) {
                 listOfCookies
-                    .filter( cookieName => this.filterFromPatterns(cookieName, cookiesPatterns)) 
-                    .map( cookieName => Cookies.remove(cookieName) )
-            }   
+                    .filter(cookieName => this.filterFromPatterns(cookieName, cookiesPatterns))
+                    .map(cookieName => Cookies.remove(cookieName))
+            }
         } catch (error) {
-            
+
         }
     }
 
     /**
      * Delete localStorage linked to the service.
-     * 
-     * @param {Service} service 
+     *
+     * @param {Service} service
      */
-    deleteLocalStorage(service){
-        if( window.localStorage ){
+    deleteLocalStorage(service) {
+        if (window.localStorage) {
             try {
                 // service.getLocalStoragePatterns()
                 const localStorageNames = Object.keys(window.localStorage)
                 const localStoragePatterns = service.getLocalStoragePatterns()
-                if( localStorageNames.length && localStoragePatterns.length ){
+                if (localStorageNames.length && localStoragePatterns.length) {
                     localStorageNames
-                        .filter( storageName => this.filterFromPatterns(storageName, localStoragePatterns)) 
-                        .map( storageName => {
+                        .filter(storageName => this.filterFromPatterns(storageName, localStoragePatterns))
+                        .map(storageName => {
                             window.localStorage.removeItem(storageName)
 
-                        } )
-                } 
+                        })
+                }
             } catch (error) {
             }
         }
@@ -333,13 +346,13 @@ class ServiceManagerClass{
 
     /**
      * Returns the list that matches patterns.
-     * 
-     * @param {string} item 
-     * @param {Array} patternsList 
+     *
+     * @param {string} item
+     * @param {Array} patternsList
      */
-    filterFromPatterns(item, patternsList){
-        for( let i in patternsList){
-            if( item.match(patternsList[i]) ){
+    filterFromPatterns(item, patternsList) {
+        for (let i in patternsList) {
+            if (item.match(patternsList[i])) {
                 return true
             }
         }
@@ -349,11 +362,10 @@ class ServiceManagerClass{
     /**
      * Toggle service
      */
-    toggleService(service){
-        if( service.isEnabled() ){
+    toggleService(service) {
+        if (service.isEnabled()) {
             this.disableService(service)
-        }
-        else{
+        } else {
             this.enableService(service)
         }
     }
