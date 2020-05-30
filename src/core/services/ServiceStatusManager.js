7bdb07b63874d3a87fc788cc436d0ff5cd67cefa
thomas.secher <thomas.secher@consertotech.pro>
2020-05-30 12:20:29 +0200

    [core/status] Stock status in localStorage first

--- src/core/services/ServiceStatusManager.js
+++ src/core/services/ServiceStatusManager.js
@@ -1,49 +1,52 @@
 import {ID} from '../tools/Tools';
+
 export const Cookies = require('js-cookie');
 
 export const ServiceStatus = {
-    enabled: 1,
-    pending: -1,
-    disabled: 0,
+	enabled: 1,
+	pending: -1,
+	disabled: 0,
 }
 
-class ServiceStatusManagerClass{
+class ServiceStatusManagerClass {
 
-    constructor(){        
-    }
+	constructor() {
+	}
 
-    getServiceStatus( service, retry=true ){
-        // If a status is defined in cookie, we return the value
-        const allStatus = this.getData()
-        if (typeof(allStatus[service.id]) !== 'undefined'){
-            return allStatus[service.id]
-        }
+	getServiceStatus(service, retry = true) {
+		// If a status is defined in cookie, we return the value
+		const allStatus = this.getData()
+		if (typeof (allStatus[service.id]) !== 'undefined') {
+			return allStatus[service.id]
+		}
 
-        if( retry ){
-            this.setServiceStatus(service, ServiceStatus.pending)
-            return this.getServiceStatus(service, false);
-        }
+		if (retry) {
+			this.setServiceStatus(service, ServiceStatus.pending)
+			return this.getServiceStatus(service, false);
+		}
 
-        return ServiceStatus.pending
-    }
+		return ServiceStatus.pending
+	}
 
-    setServiceStatus( service, status){
-        const allStatus = this.getData()
-        allStatus[service.id] = status
-        
-        Cookies.set(ID, allStatus)
-    }
+	setServiceStatus(service, status) {
+		const allStatus = this.getData()
+		allStatus[service.id] = status
+		if (localStorage) {
+			localStorage.setItem(ID, JSON.stringify(allStatus));
+		} else {
+			Cookies.set(ID, allStatus)
+		}
+	}
 
-    getData(){
-        let data = {}
-        try {
-            data = JSON.parse(Cookies.get(ID))
-        } catch (error) {      
-        }
-
-        return data
-        
-    }
+	getData() {
+		// Check localstorage first.
+		let data = localStorage.getItem(ID) || Cookies.get(ID) || {}
+		try {
+			data = JSON.parse(data)
+		} catch (error) {
+		}
+		return data
+	}
 }
 
 export const ServiceStatusManager = new ServiceStatusManagerClass()
